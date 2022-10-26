a6f91a6395c956e89ebcbb9c090e5cffe5dd4c89
Scott Laing <scott.laing@kotikan.com>
2022-10-26 15:34:15 +0100

    rotate the snake assets for directionality

--- snake-game Watch App/Source/GameScene.swift
+++ snake-game Watch App/Source/GameScene.swift
@@ -4,6 +4,10 @@ class GameScene: SKScene {
     
     var model = SnakeModel()
     var lastUpdate = 0.0
+    
+    let _90: CGFloat = .pi / 2
+    let _180: CGFloat = .pi
+    let _270: CGFloat = .pi / -2
 
     override init(size: CGSize) {
         super.init(size: size)
@@ -28,7 +32,6 @@ class GameScene: SKScene {
     override func update(_ currentTime: TimeInterval) {
         let delta = currentTime - lastUpdate
         if (delta > 0.6) {
-            print("going to do a move")
             // move snake
             model.move(withFood: true)
             removeAllChildren()
@@ -52,16 +55,58 @@ class GameScene: SKScene {
     
     private func spawnChild(_ item: SnakeElement) -> SKSpriteNode {
         let snakeImage: String
+        let rotation: CGFloat
         if item.directionNext == nil {
             snakeImage = "head"
+            switch item.directionPrevious! {
+            case .north:
+                rotation = _180
+            case .south:
+                rotation = 0.0
+            case .east:
+                rotation = _90
+            case .west:
+                rotation = _270
+            }
         } else if item.directionPrevious == nil {
             snakeImage = "tail"
+            switch item.directionNext! {
+            case .north:
+                rotation = _180
+            case .south:
+                rotation = 0.0
+            case .east:
+                rotation = _90
+            case .west:
+                rotation = _270
+            }
+        } else if item.isCorner() {
+            snakeImage = "corner"
+            switch (item.directionPrevious!, item.directionNext!) {
+            case (.west, .north), (.north, .west):
+                rotation = 0.0
+            case (.west, .south), (.south, .west):
+                rotation = _90
+            case (.east, .north), (.north, .east):
+                rotation = _270
+            case (.east, .south), (.south, .east):
+                rotation = _180
+            default:
+                rotation = 0.0
+            }
         } else {
-            snakeImage = item.isCorner() ? "corner" : "body"
+            snakeImage = "body"
+            switch item.directionNext! {
+            case .north, .south:
+                rotation = _90
+            case .east, .west:
+                rotation = 0.0
+            }
         }
 
         let snake = SnakePart(imageName: snakeImage)
         snake.position = CGPoint(x: item.position.x * 15, y: item.position.y * 15)
+        snake.zRotation = rotation
         return snake
     }
     
