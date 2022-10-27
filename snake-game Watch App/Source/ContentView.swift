0950f4a328fec290eee6dc7728d0e99bc6996662
Robert Hewitt <fuzzybear@gmail.com>
2022-10-27 14:40:55 +0100

    fixed the scroll - by removing the last stuff

--- snake-game Watch App/Source/ContentView.swift
+++ snake-game Watch App/Source/ContentView.swift
@@ -20,8 +20,8 @@ struct ContentView: View {
     
     @State var startPos : CGPoint = .zero
     @State var isSwipping = true
-    @State var scrollAmount = 1
-    @State var currentScrollAmount = 1
+    @State var scrollAmount = 1.0
+    @State var currentScrollAmount = 1.0
     
     var scene = GameScene(size: WKInterfaceDevice.current().screenBounds.size)
     
@@ -32,21 +32,19 @@ struct ContentView: View {
                 scene.tapped(CGPoint(x: tap.x, y: tap.y))
             })
             .focusable()
-            .digitalCrownRotation(detent: $scrollAmount, from: 0, through: 2, by: 1)
+            .digitalCrownRotation($scrollAmount)
             .onChange(of: scrollAmount, perform: { newValue in
                 print(newValue)
-                if newValue == 1 {
-                    return // don't rotate this change
-                } else if newValue > currentScrollAmount {
+                if newValue > currentScrollAmount {
                     print("ask to turn left")
                     scene.turn(.left) {
-                        scrollAmount = 1
+//                        scrollAmount = 1
                         print("attempting to set scrollAmount to 1")
                     }
                 } else {
                     print("ask to turn right")
                     scene.turn(.right) {
-                        scrollAmount = 1
+//                        scrollAmount = 1
                         print("attempting to set scrollAmount to 1")
                     }
                 }
